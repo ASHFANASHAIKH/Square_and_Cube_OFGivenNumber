9114047cbd6caaa1335d471d9f009d1b4779f45f
ashfana <ashfana@DESKTOP-NPR6MTA>
2021-11-13 11:55:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class SquareCube
+{
+    public static void Main(string[] args)
+    {
+        int num = 26, Square, Cube;
+        Square = num * num;
+        Cube = num * num * num;
+        Console.Write("number is :" + num);
+        Console.Write("\nSquare of " + num + " is : " +Square); 
+        Console.Write("\nCube of " + num + " is : " +Cube);
+        Console.ReadLine();
+    }
+}
